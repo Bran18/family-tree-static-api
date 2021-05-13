41d5046492a847b94c7f9c2fe5806fa2dc693d3e
Brandon Fernández <andreyfdez18@gmail.com>
2021-05-13 18:52:50 +0000

    final

--- src/main.py
+++ src/main.py
@@ -1,6 +1,3 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
@@ -8,7 +5,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, Primera, Segunda, Tercera
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +27,36 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/prueba', methods=['GET'])
+def prueba():
+    # get all the people
+    users_gen1 = Primera.query.all()
+    
+    # map the results and your list of people  inside of the all_people variable
+    result = list(map(lambda x: x.serialize(), users_gen1))
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(result), 200 
 
-    return jsonify(response_body), 200
+@app.route('/all', methods=['GET'])
+def get_all():
+    # get all the people
+    users_gen1 = Primera.query.all()
+    users_gen2 = Segunda.query.all()
+    users_gen3 = Tercera.query.all()
+    user_all = users_gen1 + users_gen2 + users_gen3
+    # map the results and your list of people  inside of the all_people variable
+    result = list(map(lambda x: x.serialize(), user_all))
+
+    return jsonify(result), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member_by_id(id):
+    # get all the people
+    item = Primera.query.get(id)
+    if item is None:
+        raise APIException('Member not found', status_code=404)
+
+    return jsonify(item.serialize()), 200
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,72 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Primera(db.Model):
+    __tablename__ = "primera"
+    # Notice that each column is also a normal Python instance attribute.
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    lastname = db.Column(db.String(250), unique=False, nullable=False)
+    age = db.Column(db.String(250), unique=False, nullable=False)
+    segunda = db.relationship('Segunda',backref='owner_primera', lazy=True)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Primera %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age,
+            "segunda": list(map(lambda x: x.serialize(), self.segunda)),
+          
+        }
+
+
+class Segunda(db.Model):
+    __tablename__ = 'segunda'
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    lastname = db.Column(db.String(250), unique=False, nullable=False)
+    age = db.Column(db.String(250), unique=False, nullable=False)
+    primera = db.Column(db.Integer, db.ForeignKey('primera.id'))
+    tercera = db.relationship('Tercera',backref='owner_segunda', lazy=True)
+
+
+    def __repr__(self):
+        return '<Segunda %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age,
+            "primera": self.primera,  
+            "tercera": list(map(lambda x: x.serialize(), self.tercera)),     
             # do not serialize the password, its a security breach
+        }
+
+class Tercera(db.Model):
+    __tablename__ = "tercera"
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    lastname = db.Column(db.String(250), unique=False, nullable=False)
+    age = db.Column(db.String(250), unique=False, nullable=False)
+    segunda = db.Column(db.Integer, db.ForeignKey('segunda.id'))
+
+    def __repr__(self):
+        return '<Tercera %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "lastname": self.lastname,
+            "age": self.age,
+            "segunda": self.segunda, 
+            
         }
